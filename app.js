e7fb30106d3fc1d18f8c78a3010280f2b79a87f3
SamRyong <zackinthebox@khu.ac.kr>
2024-05-11 02:16:17 +0900

    Asdfadsf

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const cctvRouter = require("./routers/cctvRouter");
 const wantedRouter = require("./routers/wantedRouter");
 const loginRouter = require("./routers/loginRouter");
+const req = require("express/lib/request");
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 
